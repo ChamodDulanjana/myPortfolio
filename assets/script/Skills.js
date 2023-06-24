6f8d962468f5be275ab33275bbe209030b12aa5b
ChamodDulanjana <chamodperera128@gmail.com>
2023-06-24 23:37:02 +0530

    update

--- assets/script/Skills.js
+++ assets/script/Skills.js
@@ -9,3 +9,16 @@ img.addEventListener('click', () => {
     }
 
 });
+
+
+
+/*
+var element = document.querySelector('.uil-angle-right-b');
+element.addEventListener('mouseover', function() {
+    // Code to execute when the mouse pointer enters the element
+    element.style.backgroundColor = '#ff0000'; // Change the background color on hover
+});
+element.addEventListener('mouseout', function() {
+    // Code to execute when the mouse pointer leaves the element
+    element.style.backgroundColor = '#eaeaea'; // Change the background color back to the original
+});*/

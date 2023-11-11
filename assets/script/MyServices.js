458310ba5e9a9f36b83dde8f623983f3d0519016
ChamodDulanjana <chamodperera128@gmail.com>
2023-11-11 14:43:45 +0530

    update

--- assets/script/MyServices.js
+++ assets/script/MyServices.js
@@ -38,3 +38,64 @@ span3.addEventListener('mouseover', function() {
 span3.addEventListener('mouseout', function() {
     arrow3.style.left = '2px';
 });
+
+
+
+function webDevelopment(){
+    var viewMore1 = document.getElementById("pop_up_web");
+    if (viewMore1.style.display === "block") {
+        viewMore1.style.display = 'none';
+    } else {
+        viewMore1.style.display = 'block';
+    }
+}
+
+
+function softwareDevelopment(){
+    var viewMore2 = document.getElementById("pop_up_Software");
+    if (viewMore2.style.display === "block") {
+        viewMore2.style.display = 'none';
+    } else {
+        viewMore2.style.display = 'block';
+    }
+}
+
+
+function uxDevelopment(){
+    var viewMore3 = document.getElementById("pop_up_ux");
+    if (viewMore3.style.display === "block") {
+        viewMore3.style.display = 'none';
+    } else {
+        viewMore3.style.display = 'block';
+    }
+}
+
+
+function closePopUp1(){
+    var closePopUp1 = document.getElementById("pop_up_web");
+    if (closePopUp1.style.display === "block") {
+        closePopUp1.style.display = 'none';
+    } else {
+        closePopUp1.style.display = 'block';
+    }
+}
+
+function closePopUp2(){
+    var closePopUp2 = document.getElementById("pop_up_Software");
+    if (closePopUp2.style.display === "block") {
+        closePopUp2.style.display = 'none';
+    } else {
+        closePopUp2.style.display = 'block';
+    }
+}
+
+function closePopUp3(){
+    var closePopUp3 = document.getElementById("pop_up_ux");
+    if (closePopUp3.style.display === "block") {
+        closePopUp3.style.display = 'none';
+    } else {
+        closePopUp3.style.display = 'block';
+    }
+}
+
+
--- assets/script/Skills.js
+++ assets/script/Skills.js
@@ -7,14 +7,17 @@ function toggleArrowOne() {
     var arrow1 = document.getElementById("skill_down_arrow_div1");
     var imageDiv1 = document.getElementById("skills_image_id1");
     var div3 = document.getElementById("skills_div-3");
-    if (arrow1.style.transform === "rotate(0deg)") {
-        arrow1.style.transform = "rotate(-180deg)";
-        imageDiv1.style.display = "grid";
-        div3.style.transform = "translateY(30px)";
-    } else {
+    var div02 = document.getElementById("skills_div-2");
+    if (arrow1.style.transform === "rotate(-180deg)") {
         arrow1.style.transform = "rotate(0deg)";
         imageDiv1.style.display = "none";
-        div3.style.transform = "translateY(-30px)";
+        div3.style.transform = "translateY(-10px)";
+        div02.style.display = "block";
+    } else {
+        arrow1.style.transform = "rotate(-180deg)";
+        imageDiv1.style.display = "grid";
+        div3.style.transform = "translateY(100px)";
+        div02.style.display = "none";
     }
 }
 
@@ -24,12 +27,15 @@ function toggleArrowOne() {
 function toggleArrowTwo() {
     var arrow2 = document.getElementById("skill_down_arrow_div2");
     var imageDiv2 = document.getElementById("skills_image_id2");
-    if (arrow2.style.transform === "rotate(0deg)") {
-        arrow2.style.transform = "rotate(-180deg)";
-        imageDiv2.style.display = "grid";
-    } else {
+    var div03 = document.getElementById("skills_div-3");
+    if (arrow2.style.transform === "rotate(-180deg)") {
         arrow2.style.transform = "rotate(0deg)";
         imageDiv2.style.display = "none";
+        div03.style.display = "block";
+    } else {
+        arrow2.style.transform = "rotate(-180deg)";
+        imageDiv2.style.display = "grid";
+        div03.style.display = "none";
     }
 }
 
@@ -39,12 +45,12 @@ function toggleArrowTwo() {
 function toggleArrowThree() {
     var arrow3 = document.getElementById("skill_down_arrow_div3");
     var imageDiv3 = document.getElementById("skills_image_id3");
-    if (arrow3.style.transform === "rotate(0deg)") {
-        arrow3.style.transform = "rotate(-180deg)";
-        imageDiv3.style.display = "grid";
-    } else {
+    if (arrow3.style.transform === "rotate(-180deg)") {
         arrow3.style.transform = "rotate(0deg)";
         imageDiv3.style.display = "none";
+    } else {
+        arrow3.style.transform = "rotate(-180deg)";
+        imageDiv3.style.display = "grid";
     }
 }
 

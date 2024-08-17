214bff9c1d8664dd70724d09a7b3ee581d96fb93
ChamodDulanjana <chamodperera128@gmail.com>
2024-08-17 13:04:18 +0530

    Bug fixed in section MyServices

--- assets/script/MyServices.js
+++ assets/script/MyServices.js
@@ -42,11 +42,14 @@ span3.addEventListener('mouseout', function() {
 
 
 function webDevelopment(){
+    var pop_up = document.getElementById("pop_up_main");
     var viewMore1 = document.getElementById("pop_up_web");
     if (viewMore1.style.display === "block") {
+        pop_up.style.display = "none";
         viewMore1.style.display = 'none';
         document.body.style.overflow = 'auto';
     } else {
+        pop_up.style.display = "block";
         viewMore1.style.display = 'block';
         document.body.style.overflow = 'hidden';
 
@@ -55,11 +58,14 @@ function webDevelopment(){
 
 
 function softwareDevelopment(){
+    var pop_up = document.getElementById("pop_up_main");
     var viewMore2 = document.getElementById("pop_up_Software");
     if (viewMore2.style.display === "block") {
+        pop_up.style.display = "none";
         viewMore2.style.display = 'none';
         document.body.style.overflow = 'auto';
     } else {
+        pop_up.style.display = "block";
         viewMore2.style.display = 'block';
         document.body.style.overflow = 'hidden';
     }
@@ -67,11 +73,14 @@ function softwareDevelopment(){
 
 
 function uxDevelopment(){
+    var pop_up = document.getElementById("pop_up_main");
     var viewMore3 = document.getElementById("pop_up_ux");
     if (viewMore3.style.display === "block") {
+        pop_up.style.display = "none";
         viewMore3.style.display = 'none';
         document.body.style.overflow = 'auto';
     } else {
+        pop_up.style.display = "block";
         viewMore3.style.display = 'block';
         document.body.style.overflow = 'hidden';
     }
@@ -79,33 +88,42 @@ function uxDevelopment(){
 
 
 function closePopUp1(){
+    var pop_up = document.getElementById("pop_up_main");
     var closePopUp1 = document.getElementById("pop_up_web");
     if (closePopUp1.style.display === "block") {
+        pop_up.style.display = "none";
         closePopUp1.style.display = 'none';
         document.body.style.overflow = 'auto';
 
     } else {
+        pop_up.style.display = "block";
         closePopUp1.style.display = 'block';
 
     }
 }
 
 function closePopUp2(){
+    var pop_up = document.getElementById("pop_up_main");
     var closePopUp2 = document.getElementById("pop_up_Software");
     if (closePopUp2.style.display === "block") {
+        pop_up.style.display = "none";
         closePopUp2.style.display = 'none';
         document.body.style.overflow = 'auto';
     } else {
+        pop_up.style.display = "block";
         closePopUp2.style.display = 'block';
     }
 }
 
 function closePopUp3(){
+    var pop_up = document.getElementById("pop_up_main");
     var closePopUp3 = document.getElementById("pop_up_ux");
     if (closePopUp3.style.display === "block") {
+        pop_up.style.display = "none";
         closePopUp3.style.display = 'none';
         document.body.style.overflow = 'auto';
     } else {
+        pop_up.style.display = "block";
         closePopUp3.style.display = 'block';
     }
 }

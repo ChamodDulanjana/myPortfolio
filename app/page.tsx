853009d78aff0f9c00ee659d352acae8f1b42456
chamoddulanjana <chamodperera128@gmail.com>
2025-09-11 22:44:44 +0530

    implementing about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/about";
 import Hero from "@/components/hero";
 
 
@@ -5,10 +6,11 @@ export default function Home() {
   return (
     <main className="flex w-full h-full flex-col items-center pt-14 mx-auto">
       <div>
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <div className="bg-[#f7cfd0] absolute top-[-6rem] -z-10 right-[8rem] h-[34rem] w-[34rem] rounded-full blur-[8rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[34rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
       </div>
       <Hero />
+      <About />
     </main>
   );
 }
---
+++ components/about.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import React from 'react'
+import { motion } from 'framer-motion'
+import SectionHeading from './section-heading'
+
+
+const About = () => {
+console.log('About component rendered', motion);
+  return (
+    <motion.section
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        I’m a passionate developer who loves turning ideas into meaningful digital 
+        experiences. What excites me most about programming is the problem solving 
+        journey taking a complex challenge, breaking it down, and finding elegant 
+        solutions that make a real impact.
+      </p>
+
+      <p>
+        I thrive in environments where I can learn, adapt, and collaborate, and I’m 
+        always curious about exploring new ways to grow both personally and professionally.
+        I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a software
+        developer.
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        video games and watching movies. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I'm also
+        learning how to play the guitar.
+      </p>
+    </motion.section>
+  )
+}
+
+export default About

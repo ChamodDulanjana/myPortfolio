124896d5e44fffe63251a7e16f201842b2660aa9
chamoddulanjana <chamodperera128@gmail.com>
2025-09-11 18:07:17 +0530

    add cv

--- components/hero.tsx
+++ components/hero.tsx
@@ -87,7 +87,7 @@ const Hero = () => {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack"
-          href="/CV.pdf"
+          href="/Chamod's Resume.pdf"
           download
         >
           Download CV{" "}

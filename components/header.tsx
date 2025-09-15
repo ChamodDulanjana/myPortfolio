cb812404f55f0c9d1fb3d98535af46aff415626a
chamoddulanjana <chamodperera128@gmail.com>
2025-09-16 00:23:07 +0530

    header styles utd

--- components/header.tsx
+++ components/header.tsx
@@ -45,7 +45,7 @@ export default function Header() {
 
                 {link.name === activeSection && (
                   <motion.span
-                    className="bg-[#d4e0ff] rounded-full absolute inset-0 -z-10"
+                    className="border border-gray-200 shadow-sm rounded-full absolute inset-0 -z-10"
                     layoutId="activeSection"
                     transition={{
                       type: "spring",

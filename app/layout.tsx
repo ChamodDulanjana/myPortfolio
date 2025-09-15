d05d24cc8d11f75ffb3c454ed3cfae37868f6174
chamoddulanjana <chamodperera128@gmail.com>
2025-09-15 14:08:46 +0530

    build ConditionalHeader to avoid display header unwanted pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ActiveSectionContextProvider from "@/context/active-section-context";
-import Header from "@/components/header";
+import ConditionalHeader from "@/components/conditional-header";
 
 // const inter = Inter({
 //   subsets: ["latin"],
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={`bg-gray-50 text-gray-950 relative `} >
         <ActiveSectionContextProvider>
-          <Header />
+          <ConditionalHeader />
           {children}
         </ActiveSectionContextProvider>
       </body>
